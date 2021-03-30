2fb671e84b5e3696e820f235d99f20cb500c2b7e
Basit-Balogun10 <basitbalogun10@gmail.com>
2021-03-31 00:55:00 +0100

    Created django views, html, css and JS files for sub pages and I created a header and footer html template for all other pages.
    Signed-off-by: Basit-Balogun10 <basitbalogun10@gmail.com>

--- sni_app/urls.py
+++ sni_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # path('', views.home, name='home'),
+    path('organizer', views.organizer, name='organizer'),
+    path('speakers', views.speakers, name='speakers'),
+    path('sponsors', views.sponsors, name='sponsors'),
+    path('venue', views.venue, name='venue'),
 ]
--- sni_app/views.py
+++ sni_app/views.py
@@ -8,3 +8,28 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'sni_app/home.html')
     # return HttpResponse('View is working')
+
+
+def organizer(request):
+    return render(request, 'sni_app/organizer.html')
+    # return HttpResponse('Organizer View is working')
+
+
+def schedule(request):
+    # return render(request, 'sni_app/schedule.html')
+    return HttpResponse('Schedule View is working')
+
+
+def speakers(request):
+    return render(request, 'sni_app/speakers.html')
+    # return HttpResponse('Speakers View is working')
+
+
+def sponsors(request):
+    return render(request, 'sni_app/sponsors.html')
+    # return HttpResponse('Sponsors View is working')
+
+
+def venue(request):
+    return render(request, 'sni_app/venue.html')
+    # return HttpResponse('Venue View is working')
