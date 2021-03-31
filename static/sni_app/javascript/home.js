c3803be7190282765ebfcabff845662a63882216
Basit-Balogun10 <basitbalogun10@gmail.com>
2021-03-31 23:57:47 +0100

    1) Removed the Jump to Top and Bottom buttons and other codes related to them in css, js and html 2) Removed the 'fixed' position on nav menu bar 3) Deleted redundant lines of code for previous 'hover to dropdown nav menu' feature in small screens.
    Signed-off-by: Basit-Balogun10 <basitbalogun10@gmail.com>

--- static/sni_app/javascript/home.js
+++ static/sni_app/javascript/home.js
@@ -30,57 +30,8 @@ if (url == "http://127.0.0.1:8000/") {
     i += 1;
   }
 }
-//Get the button:
-topbutton = document.getElementById("toTop-btn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  topscrollFunction(), bottomscrollFunction(), changePos();
-};
-
-function topscrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    topbutton.style.display = "block";
-  } else {
-    topbutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-bottombutton = document.getElementById("toBottom-btn");
 myHeader = document.getElementById("header");
 navBar = document.getElementById("navBar");
-
-function bottomscrollFunction() {
-  if (
-    document.documentElement.scrollTop ==
-      document.documentElement.scrollHeight - window.innerHeight ||
-    document.body.scrollTop == document.body.scrollHeight - window.innerHeight
-  ) {
-    bottombutton.style.display = "none";
-    if (window.innerWidth >= 970) {
-      navBar.style.top = "-100rem";
-      //      myHeader.style.display = "none";
-    }
-  } else {
-    bottombutton.style.display = "block";
-    navBar.style.top = "0";
-    //    myHeader.style.display = "block";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function bottomFunction() {
-  window.scrollTo(
-    0,
-    document.body.scrollHeight || document.documentElement.scrollHeight
-  );
-}
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < routes.length; i++) {
@@ -98,7 +49,6 @@ function routesDisplay() {
   if (ul.className === "navigation") {
     ul.className += " responsive";
     dropdown.className += " responsive";
-    bottombutton.style.display = "none";
   } else {
     ul.className = "navigation";
     dropdown.className = "icon";
@@ -106,48 +56,3 @@ function routesDisplay() {
 }
 
 footer = document.getElementById("pageFooter");
-
-var observer = new IntersectionObserver(
-  function (entries) {
-    if (entries[0].isIntersecting === true) {
-      if (window.innerWidth < 970) {
-        navBar.style.display = "none";
-        // navBar.style.top = "-100rem";
-        bottombutton.style.marginTop = "-10rem";
-      }
-    } else {
-      navBar.style.display = "block";
-      bottombutton.style.marginTop = "0rem";
-      // navBar.style.top = "0";
-    }
-  },
-  { threshold: [0] }
-);
-
-function changePos() {
-  if (window.innerHeight < 330) {
-    bottombutton.style.marginRight = "40rem";
-  } else {
-    bottombutton.style.marginRight = "0rem";
-  }
-}
-
-observer.observe(footer);
-
-// const [x1, x2, x3, x4] = 0;
-
-// function distanceBetweenElements(navBar, footer) {
-//   let distance = -1;
-
-//   const x1 = navBar.offset().top;
-//   const y1 = navBar.offset().left;
-//   const x2 = footer.offset().top;
-//   const y2 = footer.offset().left;
-//   const xDistance = x1 - x2;
-//   const yDistance = y1 - y2;
-
-//   distance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
-
-//   console.log(distance);
-//   return distance;
-// }
